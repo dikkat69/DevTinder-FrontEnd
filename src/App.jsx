d4d74cd70cdf82c544f733f492f652bb5202f484
dikkat69 <ui23cs69@iiitsurat.ac.in>
2026-02-19 00:43:03 +0530

    Tailwind + DaisyUI + Basic Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Body from "./Components/Body"
+import Profile from "./Components/Profile"
+import Login from "./Components/Login"
+
+
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+
+    <BrowserRouter basename="/">
+    <Routes>
+      <Route path="/" element={<Body/>} >
+        <Route path="/login" element={<Login/>} />
+        <Route path="/profile" element={<Profile/>} />
+      </Route>
+    </Routes>
+    </BrowserRouter>
     </>
-  )
-}
+  )}
 
 export default App
